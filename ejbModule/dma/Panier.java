bd8e0b564d2c14c7104c5c0b9880cad5ae219bb1
Karin Touchie <ktouchie@gmail.com>
2016-07-05 17:30:55 +0200

    Add methods for Panier.java

--- ejbModule/dma/Panier.java
+++ ejbModule/dma/Panier.java
@@ -1,25 +1,32 @@
 package dma;
 
+import java.util.ArrayList;
+
 import javax.ejb.Stateful;
 
 import domaine.Article;
 
 @Stateful
 public class Panier implements PanierRemote{
+	
+	ArrayList<Article> panier = new ArrayList<Article>();
 
 	@Override
 	public void ajouter(Article article) {
-		
+		panier.add(article);
 	}
 
 	@Override
 	public void consulter() {
 		
+		for (Article a : panier) {
+			
+		}
 	}
 
 	@Override
 	public void vider() {
-		
+		panier.clear();
 	}
 
 }
