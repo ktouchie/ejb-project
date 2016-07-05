d5c4d9f21073fa1231c18758eea78cd818a5a5e5
Karin Touchie <ktouchie@gmail.com>
2016-07-05 13:44:48 +0200

    Create Panier

---
+++ ejbModule/dma/Panier.java
@@ -0,0 +1,26 @@
+package dma;
+
+import javax.ejb.Stateful;
+
+@Stateful
+public class Panier implements PanierRemote{
+
+	@Override
+	public void ajouter(Article article) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void consulter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vider() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
