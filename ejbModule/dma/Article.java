48c70704098c9a0cafbdd01f2769fc13487e1035
Karin Touchie <ktouchie@gmail.com>
2016-07-05 13:55:00 +0200

    Create Article class

---
+++ ejbModule/dma/Article.java
@@ -0,0 +1,61 @@
+package dma;
+
+public class Article {
+	
+	public String id;
+	public String nom;
+	public double prixHT;
+	public double remise;
+	public int quantite;
+	
+	public Article(String id, String nom, double prixHT, double remise,
+			int quantite) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prixHT = prixHT;
+		this.remise = remise;
+		this.quantite = quantite;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrixHT() {
+		return prixHT;
+	}
+
+	public void setPrixHT(double prixHT) {
+		this.prixHT = prixHT;
+	}
+
+	public double getRemise() {
+		return remise;
+	}
+
+	public void setRemise(double remise) {
+		this.remise = remise;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+}
--- ejbModule/dma/Panier.java
+++ ejbModule/dma/Panier.java
@@ -7,19 +7,16 @@ public class Panier implements PanierRemote{
 
 	@Override
 	public void ajouter(Article article) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void consulter() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void vider() {
-		// TODO Auto-generated method stub
 		
 	}
 
